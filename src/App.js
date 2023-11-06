dc48922aa69946a06e996936e9bace6a9c6eddf4
Kisho (Sho) Tamura <sho@mango-byte.com>
2023-11-06 11:02:56 +0700

    add loop and if

--- src/App.js
+++ src/App.js
@@ -34,22 +34,15 @@ export function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className='status'>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} /> 
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-      
+      {
+        [0,3,6].map((value) => 
+          <div className="board-row">
+            {
+              [0,1,2].map((index) => <Square value={squares[value + index]} onSquareClick={() => handleClick(value + index)} />)
+            }
+          </div>
+        )
+      }
     </>
   ) ;
 }
@@ -80,7 +73,9 @@ export default function Game() {
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {
+          move === history.length - 1 ? <div onClick={() => jumpTo(move)}>{description}</div> : <button onClick={() => jumpTo(move)}>{description}</button>
+        }
       </li>
     );
   });
